1c3452074695290eed6a5ae2a9e34d364cbdd684
keuranta <keuranta@gmail.com>
2016-06-10 12:19:11 +0200

    all

--- src/BlogBundle/BlogBundle.php
+++ src/BlogBundle/BlogBundle.php
@@ -4,6 +4,16 @@ namespace BlogBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+
+use Doctrine\DBAL\Event\Listeners\MysqlSessionInit;
+
 class BlogBundle extends Bundle
 {
+	public function boot() {
+		$this->container
+		->get('doctrine.orm.entity_manager')
+		->getEventManager()
+		->addEventSubscriber(new MysqlSessionInit('utf8', 'utf8_unicode_ci'));
+
+	}
 }
